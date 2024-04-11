a656200074eb10cf5622e1b7887306566cb91ac4
Snarsy <uleberg.lars@gmail.com>
2024-04-11 10:01:54 +0200

    Adding shit

---
+++ .arduino/output/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -0,0 +1,1 @@
+# 1 "C:\\Users\\larsu\\Desktop\\Skolearbeid\\Andre semester\\IELS2021\\Designprosjekt 3\\IELS-Prosjekt-2024\\Mainfile\\Mainfile.ino"
---
+++ .arduino/output/sketch/Mainfile.ino.cpp
@@ -0,0 +1,3 @@
+#include <Arduino.h>
+#line 1 "C:\\Users\\larsu\\Desktop\\Skolearbeid\\Andre semester\\IELS2021\\Designprosjekt 3\\IELS-Prosjekt-2024\\Mainfile\\Mainfile.ino"
+
