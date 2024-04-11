b103ee6cb899e3674718c97fa3f83f4a6abfad63
Snarsy <uleberg.lars@gmail.com>
2024-04-11 14:30:48 +0200

    Update Garage.ino

--- Garage/Garage.ino
+++ Garage/Garage.ino
@@ -16,6 +16,7 @@ bool sjekk1=0;
 bool sjekk2=0;
 bool drive = 1;
 bool inGarage = 0;
+bool onRoad = 0;
 
 void turndeg(int tilverdi)//Denne funksjonen roterer bilen i 90 grader ved hjelp av gyroen. Den har 87 grader ettersom dette er hva 90 grader er lik på skolens gulv.
 //Derimot med mer friksjon så vil man bruke 90 grader istedenfor 87 for 90.
@@ -55,6 +56,23 @@ void turnreset(){
     drive=1;
 }
 
+const int followLinemaxSpeed = 200;
+int lastError = 0;
+void driveLinePID()
+{ //Linjefølging tatt fra Kevin Pololu. 
+    int16_t position = lineSensors.readLine(lineSensorValues); //Leser linjeesensor. 
+    int16_t error = position - 2000; //Finnet ut om det er venstre eller høyre som er mer utenfor linjen.
+    int16_t speedDifference = error / 1 + 4 * (error - lastError); 
+
+    lastError = error;
+
+    int leftSpeed = followLinemaxSpeed + speedDifference;
+    int rightSpeed = followLinemaxSpeed - speedDifference;
+    leftSpeed = constrain(leftSpeed, 0, (int16_t)followLinemaxSpeed); //Constrain for å ikke få minus, og ikke over 400 verdi. 
+    rightSpeed = constrain(rightSpeed, 0, (int16_t)followLinemaxSpeed);
+
+    motors.setSpeeds(leftSpeed, rightSpeed);
+}
 
 void setup(){
   lineSensors.initFiveSensors();
@@ -65,22 +83,27 @@ void setup(){
 void loop(){
   if(buttonA.isPressed()){
     delay(500);
-    motors.setSpeeds(100,100);
+    onRoad = 1;
+  }
+  if(onRoad == 1){
+    driveLinePID();
   }
   readSensors();
   if (inGarage == 0){
     if(aboveLine(0) && aboveLine(1) && aboveLine(2) && aboveLine(3) && aboveLine(4)){
     delay(400);
     motors.setSpeeds(0,0);
+    onRoad = 0;
     inGarage = 1;
     delay(300);
     }
   }
   else if (inGarage == 1){
-    if(aboveLine(2) && aboveLine(3) && aboveLine(4)){
-      inGarage = 0;
+    if(aboveLine(2) && aboveLine(3) && aboveLine(4) && onRoad == 1){
       delay(400);
       motors.setSpeeds(0,0);
+      inGarage = 0;
+      onRoad = 0;
       turndeg(87);
     }
   }
