530e78dcda3ce8361b6db4467387bd22716e846f
Fosen1 <ffosen@hotmail.com>
2024-05-30 16:11:56 +0200

    Bomstasjon esp32 fungerer

--- bomstasjonEsp32/bomstasjonEsp32.ino
+++ bomstasjonEsp32/bomstasjonEsp32.ino
@@ -1,28 +1,27 @@
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
 
-// Define the GPIO pin you want to use
-const uint16_t kIrLedPin = 17;  // ESP32 pin GPIO 17
 
-IRsend irsend(kIrLedPin);
+const uint16_t IRPin = 32;  // ESP32 pin GPIO 32
+
+IRsend irsend(IRPin);
 
 void setup() {
-  // Initialize serial communication
+  
   Serial.begin(115200);
 
-  // Enable the IR LED
+  //Start ir sending
   irsend.begin();
   Serial.println("IR LED enabled");
 }
 
 void loop() {
-  // Send a NEC IR signal (example address and command)
   uint16_t address = 0x45678912;
   uint16_t command = 32;
-  irsend.sendNEC(address, command);
+  irsend.sendNEC(address, command); //Send til addresse og command. 
 
   Serial.println("Sent NEC IR signal");
 
-  // Delay before sending the next signal
-  delay(100);  // 2 seconds delay
+  
+  delay(100);  
 }
