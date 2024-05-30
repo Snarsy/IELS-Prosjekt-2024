6ed3ba17817c59486ab60e778ee3ec9ea38f5b91
Snarsy <uleberg.lars@gmail.com>
2024-05-30 10:51:28 +0200

    Har lag til to filer. En for sending av IR fra Zumo bilen og en for mottak til Arduino.

---
+++ ZumoIrSender/ZumoIrSender.ino
@@ -0,0 +1,17 @@
+// De nedenfor er til for å initialisere sendingen av ir signal. Dette lar oss ikke sende spesifikke IR signal, men et med koden FFFFFFFF
+#include "Zumo32u4IRsender.h"
+#define DEVICE_ID 35
+uint64_t lastTime = 0;
+Zumo32U4IRsender ZumoIrSender(DEVICE_ID, RIGHT_IR);
+
+void setup(){
+    Serial.begin(9600);
+    delay(3000);
+}
+
+void loop(){
+    if(millis()-lastTime > 1000){
+        lastTime = millis();
+        ZumoIrSender.send(1);
+    }
+}
