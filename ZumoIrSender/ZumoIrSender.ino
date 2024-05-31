798582f5ad0ed1b1eff35b528dc3960c971148cb
Snarsy <uleberg.lars@gmail.com>
2024-05-31 13:39:24 +0200

    Endret for å få ir til å sende

--- ZumoIrSender/ZumoIrSender.ino
+++ ZumoIrSender/ZumoIrSender.ino
@@ -1,14 +1,15 @@
-// De nedenfor er til for å initialisere sendingen av ir signal. Dette lar oss ikke sende spesifikke IR signal, men et med koden FFFFFFFF
 #include "Zumo32u4IRsender.h"
-#define DEVICE_ID 35
-uint64_t lastTime = 0;
-Zumo32U4IRsender ZumoIrSender(DEVICE_ID, RIGHT_IR);
 
-void setup(){
-    Serial.begin(9600);
-    delay(3000);
+#define DEVICE_ID 0x01
+#define DIRECTION RIGHT_IR
+
+Zumo32U4IRsender ZumoIrSender(DEVICE_ID, DIRECTION);
+
+void setup() {
+  Serial.begin(9600);
 }
 
-void loop(){
-    drivelinePID
+void loop() {
+  ZumoIrSender.send(0x11);
+  delay(1000);
 }
