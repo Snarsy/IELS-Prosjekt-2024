7f94ef73cb9bf3eafa7d82e860ca64e8a0c71d9b
EinarPF <perdomoeinar@gmail.com>
2024-06-04 18:13:29 +0200

    Tatt ut av bomstasjon

--- Sluttkode/chargingStation/chargingStationUNO/chargingStationUNO.ino
+++ Sluttkode/chargingStation/chargingStationUNO/chargingStationUNO.ino
@@ -112,3 +112,39 @@ void loop(){
     wireNumber();
     skjermValg();
 }
+
+
+#include <Arduino.h>
+#include <SPI.h>
+#include "U8glib.h" // include the universal graphcs library
+
+//U8G2_SSD1306_128X64_NONAME_1_4W_SW_SPI display(U8G2_R0, /* clock=/ 13, / data=/ 11, / cs=/ 10, / dc=/ 9, / reset=*/ 8);
+int batteryCharge;
+ 
+U8GLIB_SSD1306_128X64 u8g(12, 11, 10, 9, 8);    // initialise a u8g display object
+//clk = 12, mosi = 11, cs = 10, dc = 9, res = 8.  på arduino
+
+//U8GLIB_SSD1306_128X64 u8g(19, 23, 18, 17, 27);    // initialise a u8g display object
+//clk = 19, mosi = 23, cs = 18, dc = 17, res = 27. på esp32
+
+void setup() {
+  u8g.begin();
+  u8g.setFont(u8g_font_6x10);  //unifont
+}
+
+void skjerm(){
+  u8g.firstPage();
+  do{
+    u8g.drawStr( 35, 10, "Ladeprosent");
+    u8g.drawStr( 60, 30, "batteryCharge");
+    u8g.drawStr(7, 60, "Avbryt");
+    u8g.drawStr(100, 60, "Kjop");
+
+    }while(u8g.nextPage());
+  
+}
+
+void loop() {
+  skjerm();
+}
+//Fjern neopixel
